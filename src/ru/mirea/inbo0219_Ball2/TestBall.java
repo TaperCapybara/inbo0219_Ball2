3b77f616eb28c3ce44b994cd7ff5d9e638adcd71
Nate <nigllemmesmash@gmail.com>
2020-09-21 16:17:05 +0300

    Initial commit

---
+++ src/ru/mirea/inbo0219_Ball2/TestBall.java
@@ -0,0 +1,10 @@
+package ru.mirea.inbo0219_Ball2;
+
+public class TestBall {
+    public static void main(String[] args) {
+        Ball b1 = new Ball(140, 30);
+        System.out.println(b1);
+        b1.move(50,6415);
+        System.out.println(b1);
+    }
+}
